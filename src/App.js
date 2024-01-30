3e1690d2e765d5744531e8efe34bf880af94a727
Elizabeth Magie <lizzieemagie@gmail.com>
2024-01-30 09:57:30 -0800

    Fleshed out App component, adding search and playlistName

--- src/App.js
+++ src/App.js
@@ -8,3 +8,26 @@ import SearchBar from './SearchBar.js';
 import SearchRestults from './SearchResults.js';
 import Playlist from './Playlist.js';
 
+
+export default function App() {
+
+    // State variables for search, tracks, and playlist name. Use naming convention from relevant components.
+    const [searchResults, setSearchResults] = useState([]);
+    const [tracks, setTracks] = useState([]);
+    const [playlistName, setPlaylistName] = useState('new playlist');
+
+    // Uses SpotifyLink.search to search for entered song and set state variable searchResults
+    const search = useCallback((song) => {
+        SpotifyLink.search(song).then(setSearchResults);
+    }, []);
+
+    // Set state variable playlistName to entered playlist name
+    const newPlaylistName = useCallback((name) => {
+        setPlaylistName(name)
+    }, []);
+    
+    // Add or remove tracks and update state variables
+
+
+    // Save playlist to Spotify via SpotifyLink
+};

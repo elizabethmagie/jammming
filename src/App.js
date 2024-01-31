08f44225ab5b53bc9048dfb018cafbe37074d1b9
Elizabeth Magie <lizzieemagie@gmail.com>
2024-01-30 17:00:07 -0800

    Finished app.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 // Import other components
 import SpotifyLink from './SpotifyLink';
 import SearchBar from './SearchBar.js';
-import SearchRestults from './SearchResults.js';
+import SearchResults from './SearchResults.js';
 import Playlist from './Playlist.js';
 
 
@@ -27,7 +27,46 @@ export default function App() {
     }, []);
     
     // Add or remove tracks and update state variables
+    const addTrack = useCallback((track) => {
+        if (tracks.some((savedTrack) => savedTrack.id === track.id)){
+            return;
+        };
+        setTracks((prevTracks) => [...prevTracks, track]);
+    }, [tracks]);
+
+    const removeTrack = useCallback((track) => {
+        setTracks((prevTracks).filter((currentTrack) => currentTrack.id !== track.id));
+    }, []);
 
 
     // Save playlist to Spotify via SpotifyLink
+    const savePlaylist = useCallback(() => {
+        const trackUris = tracks.map((track) => track.uri);
+        SpotifyLink.savePlaylist(playlistName, trackUris).then(() => {
+            setPlaylistName('new playlist');
+            setTracks([]);
+        });
+    }, [playlistName, tracks]);
+
+
+    // App render
+    return (
+        <div>
+            <h1>jammming</h1>
+            <h3>create custom playlists and save to spotify</h3>
+            <div className='App' >
+                <SearchBar onSearch={search} />
+                <div className='App-Playlist'>
+                    <SearchResults searchResults={searchResults} onAdd={addTrack} />
+                    <Playlist
+                        playlistName={playlistName}
+                        playlistTracks={tracks}
+                        onNameChange={newPlaylistName}
+                        onRemove={removeTrack}
+                        onSave={savePlaylist}
+                    />
+                </div>
+            </div>
+        </div>
+    );
 };
